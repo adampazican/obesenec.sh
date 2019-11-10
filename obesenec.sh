6bfe2348a017cbce6b23e16f1c2c014778e67386
Adam Pažičan <pazicanadam@protonmail.com>
2019-11-10 21:09:28 +0100

    commit 2

--- obesenec.sh
+++ obesenec.sh
@@ -1,6 +1,10 @@
-#Obesenec je vzdelávacia hra, ktorá učí nové slová alebo slovné spojenia a tak rozširuje slovnú zásobu a vedomosti. Cieľom hry je uhádnuť slovo (slovné spojenie) pričom máte iba obmedzený počet chybných krokov. Zo slova poznáme začiatočné, koncové písmeno a celkový počet znakov. Ostatné písmena hádame stlačením klávesy pre daný znak. Každé správne uhádnuté písmeno sa zobrazí v hľadanom slove. Pokiaľ zadáme nesprávny znak, pribudne dielik na šibenicu pre obesenca. Hra končí uhádnutím slova, alebo vybudovaním šibenice s obesencom.  
+#Obesenec je vzdelávacia hra, ktorá učí nové slová alebo slovné spojenia a tak rozširuje slovnú zásobu a vedomosti. Cieľom hry je uhádnuť slovo (slovné spojenie) pričom máte iba obmedzený počet 
+#chybných krokov. Zo slova poznáme začiatočné, koncové písmeno a celkový počet znakov. Ostatné písmena hádame stlačením klávesy pre daný znak. Každé správne uhádnuté písmeno sa zobrazí v 
+#hľadanom slove. Pokiaľ zadáme nesprávny znak, pribudne dielik na šibenicu pre obesenca. Hra končí uhádnutím slova, alebo vybudovaním šibenice s obesencom.  
 
-#Vytvorte jednoduchú bash aplikáciou ovládanú pomocou menu. Hru je možné prerušiť hru a znovu pokračovať. Po prerušení hry sa dostáva spať do menu. Pri spustení hry sa načíta náhodné slovíčko zo slovníka (súbor/funkcia) a vykresli sa začiatočný stav šibenice spolu s počtom znakov ktoré hádame. Po stlačení znaku sa obrazovka prekresli. Akékoľvek rozšírenia – počítanie skóre, modifikácie, rozšírenia levelov sú povolené.
+#Vytvorte jednoduchú bash aplikáciou ovládanú pomocou menu. Hru je možné prerušiť hru a znovu pokračovať. Po prerušení hry sa dostáva spať do menu. Pri spustení hry sa načíta náhodné slovíčko 
+#zo slovníka (súbor/funkcia) a vykresli sa začiatočný stav šibenice spolu s počtom znakov ktoré hádame. Po stlačení znaku sa obrazovka prekresli. Akékoľvek rozšírenia – počítanie skóre, 
+#modifikácie, rozšírenia levelov sú povolené.
 
 #Náhodné číslo vieme získať pomocou $RANDOM. Príklad použitia: https://coderwall.com/p/s2ttyg/random-number-generator-in-bash
 #Pracujte každý samostatne.
@@ -11,7 +15,8 @@ let zivot
 slova=(bratislava zilina "kysucke nove mesto")
 slova_length=${#slova[*]}
 
-let running
+let running=false
+let slow_render
 
 let rand
 let vybrane_slovo
@@ -25,6 +30,7 @@ function init() {
 	chyby=0
 	zivot=5	
 	running=true
+	slow_render=true
 
 	rand=$(( $RANDOM % $slova_length ))
 	vybrane_slovo=${slova[$rand]}
@@ -48,38 +54,54 @@ function init() {
 
 }
 
+function echoif() {
+	word=$1
+	condition=$2
+
+	if [ $slow_render == true ]
+	then
+		sleep .3
+	fi
+
+	if [ $condition == 1 ]
+	then
+		echo "$word"
+	else
+		echo ${word:0:1}
+	fi
+}
 
 
 function vykresliHraciuPlochu() {
-	echo "____________"
-	echo "|          |"
-	echo "|"
-	echo "|"
-	echo "|"
-	echo "|"
-	echo "|"
-	echo "|"
-	echo "|_____________"
-	echo "|             |"
-	echo "|             |"
-	echo "|_____________|"
-	echo "Skore: ${skore}  Zivot: ${zivot}    ${skore}/$((vybrane_slovo_length-vybrane_slovo_special_chars))     ${hadane_slovo}"
+	echo   "____________"
+	echoif "|          |"   $((zivot < 5))
+	echoif "|          o"   $((zivot < 4))
+	echoif "|         /|\\" $((zivot < 3))
+	echoif "|          |"   $((zivot < 2))
+	echoif "|         / \\" $((zivot < 1))
+	echo   "|_____________"
+	echo   "|             |"
+	echo   "|             |"
+	echo   "|_____________|"
+	echo   "Skore: ${skore}  Zivot: ${zivot}    ${skore}/$((vybrane_slovo_length-vybrane_slovo_special_chars))     ${hadane_slovo}"
+	slow_render=false
 }
 
 function spracujTah() {
 	hladane_pismeno=$1
-	if [ $1 == h ]
+	if [ $1 == "!p" ]
 	then
-		echo helpscreen 
-	elif [ $1 == r ]
+		return 22
+	elif [ $1 == "!r" ]
 	then
 		init
+		return
 	fi
 
 	multiplier=0
 	for (( index=0; index < $vybrane_slovo_length; index++ ))
 	do
-		if [ ${vybrane_slovo:index:1} == $hladane_pismeno ] && [  ${hadane_slovo:index:1} == _ ]
+		if [ "${vybrane_slovo:index:1}" == $hladane_pismeno ] && [  "${hadane_slovo:index:1}" == _ ]
 		then
 			((multiplier++))
 			hadane_slovo="${hadane_slovo:0:index}$hladane_pismeno${hadane_slovo:index+1}"
@@ -94,29 +116,89 @@ function spracujTah() {
 	fi
 }
 
-init
 
-while [ running=true ]
-do
-	clear
-	vykresliHraciuPlochu
-	let pismeno
-	read -p "Zadaj pismeno:" pismeno
-	spracujTah $pismeno
-
-	if [ $zivot -eq 0 ]
-	then
-		echo "Prehral si!"
-		break
-	fi
-
-	if [ $skore -eq $((vybrane_slovo_length-vybrane_slovo_special_chars)) ]
-	then 
-		echo "Vyhral si!"
+function gameLoop() {
+	while [ $running == true ]
+	do
 		clear
 		vykresliHraciuPlochu
-		break
+		let pismeno
+		read -p "Zadaj pismeno:" pismeno
+		spracujTah $pismeno
+
+		if [ $? -eq 22 ]
+		then
+			break
+		fi
+
+		if [ $zivot -eq 0 ]
+		then
+			clear
+			vykresliHraciuPlochu
+			echo "Prehral si!"
+			running=false
+			break
+		fi
+
+		if [ $skore -eq $((vybrane_slovo_length-vybrane_slovo_special_chars)) ]
+		then 
+			clear
+			vykresliHraciuPlochu
+			echo "Vyhral si!"
+			running=false
+			break
+		fi
+	done
+
+	startMenu
+}
+
+function startMenu() {
+	echo "Obesenec ultimate"
+	start_menu=()
+	if [ $running == true ]
+	then 
+		start_menu+=("Pokracovat")
 	fi
-done
+	start_menu+=(
+		"Nova hra"
+		"Pomoc"
+		"Koniec"
+	)
 
+	PS3='Vyber akciu: '
 
+	select option in "${start_menu[@]}"
+	do    
+		case $option in 
+			"Pokracovat")
+				gameLoop
+				;;
+			"Nova hra")
+				init
+				gameLoop
+				break 
+				;;
+			"Pomoc")
+				if [ $running == true ]
+				then
+					echo "Vyberom moznosti Pokracovat pokracujes v rozohranej hre"
+				fi
+				echo "Novu hru zapnes vyberom moznosti Nova hra"
+				echo "V hre sa hadaju slova po jednom pismenku, pricom na zaciatku su zname len prve a posledne pismeno slova a jeho dlzka"
+				echo "Hrac ma 5 zivotov a po vycerpani zivotov hra konci prehrou"
+				echo "Po uhadnuti vsetkych pismen slova hra konci vyhrou"
+				echo "ingame prikazy: "
+				echo "!p -> pauza"
+				echo "!r -> restart"
+				echo "Vyberom moznosti Pomoc zobrazis tuto napovedu"
+				echo "Vyberom moznosti Koniec ukoncis hru"
+				;;
+			"Koniec") 
+				break ;;
+		esac
+	done
+}
+
+clear
+startMenu
